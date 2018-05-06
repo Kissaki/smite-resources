2f3fee737ee5412cb5a5efed9ba99c2d7ce31aab
Jan Klass <kissaki@posteo.de>
2018-05-06 12:13:23 +0200

    Add godstats, tried with d3

---
+++ godstats.js
@@ -0,0 +1,132 @@
+var KEY_ASPL = 'AttackSpeedPerLevel'
+var KEY_AS = 'AttackSpeed'
+var KEY_HP = 'Health'
+var KEY_HPPL = 'HealthPerLevel'
+var KEY_HP5 = 'HealthPerFive'
+var KEY_HP5PL = 'HP5PerLevel'
+var KEY_NAME = 'Name'
+var KEY_PANTHEON = 'Pantheon'
+var KEY_ROLES = 'Roles'
+var KEY_TYPE = 'Type'
+var KEY_MP = 'Mana'
+var KEY_MPPL = 'ManaPerLevel'
+var KEY_MP5 = 'ManaPerFive'
+var KEY_MP5PL = 'MP5PerLevel'
+var KEY_SPEED = 'Speed'
+
+var DATA_SRC = 'data/gods.json'
+var request = new XMLHttpRequest();
+request.open('GET', DATA_SRC);
+request.responseType = 'json';
+request.send();
+request.onload = function(e) {
+  var req = e.target
+  var res = req.response
+  handleGods(res)
+}
+function compareSpeed(a, b) {
+    return a.Speed - b.Speed
+}
+function compareHealth(a, b) {
+    return a.Health - b.Health
+}
+function createBar(value, min, max) {
+    var diff = max - min
+    var barWidth = 100 + (value - min) / diff * 100
+    return '<div style="float:left; width:' + barWidth + 'px; background-color:' + rgbString(255, 255, 255) + ';">' + value + '</div>'
+}
+function handleGods(gods) {
+    gods.sort(compareSpeed)
+
+    var speeds = document.querySelector('#speeds tbody')
+    var godsBySpeed = {}
+    gods.forEach(function(god){
+        godsBySpeed[god.Speed] = godsBySpeed[god.Speed] || []
+        godsBySpeed[god.Speed].push(god)
+    })
+    var godsBySpeedArray = [];
+    for (var speed in godsBySpeed){
+        godsBySpeedArray.push(godsBySpeed[speed])
+        // var elRow = document.createElement('tr')
+        // var elLabel = document.createElement('td')
+        // elLabel.classList.add('label')
+        // elLabel.textContent = speed
+        // elRow.appendChild(elLabel)
+        // var elBoxCell = document.createElement('td')
+        // var elBox = document.createElement('div')
+        // elBox.classList.add('box')
+        // elBox.style.width = speed + 'px'
+        // elBoxCell.appendChild(elBox)
+        // elRow.appendChild(elBoxCell)
+        // var cellGods = document.createElement('td')
+        // var elGods = document.createElement('ul')
+        // elGods.classList.add('gods')
+        // godsBySpeed[speed].forEach(function(god){
+        //     var elGod = document.createElement('li')
+        //     elGod.textContent = god.Name
+        //     elGods.appendChild(elGod)
+        // })
+        // cellGods.appendChild(elGods)
+        // elRow.appendChild(cellGods)
+        // speeds.appendChild(elRow)
+    }
+    // d3.select('#speeds tbody')
+    //     .selectAll('tr')
+    //     .data(godsBySpeedArray)
+    //     // .data([4, 8, 15, 16, 23, 42])
+    //     .enter()
+    //     .append('tr')
+    //     .append('td')
+    //     .text(function(d){return(d)})
+    //     .exit()
+
+    var tbody = d3.select('#speeds tbody')
+    tr = tbody.selectAll("tr")
+        .data(godsBySpeedArray)
+        .enter().append("tr")
+    tr.exit().remove()
+    var td = tr.selectAll('td')
+        .data(function(d){console.log(d); return [d[0].Speed, d];})
+        .text(function(d){ return d;})
+    td.enter().append('td').text(function(d){return d})
+    td.exit().remove()
+
+
+    gods.sort(compareHealth)
+    var healths = document.querySelector('#healths')
+    var min = null
+    var max = null
+    var minPL = null
+    var maxPL = null
+    for (var i in gods) {
+        var god = gods[i]
+
+        var field = god.Health
+        min = min === null || field < min ? field : min
+        max = max === null || field > max ? field : max
+
+        var fieldPL = god.HealthPerLevel
+        var field20 = fieldPL * 20
+        minPL = (minPL === null || field20 < minPL) ? field20 : minPL
+        maxPL = (maxPL === null || field20 > maxPL) ? field20 : maxPL
+    }
+    for (var i in gods) {
+        var god = gods[i]
+        var g = document.createElement('li')
+
+        var field = god.Health
+        var cBar = createBar(field, min, max)
+
+        var fieldPL = god.HealthPerLevel
+        var field20 = fieldPL * 20
+        var cBarPL = createBar(field20, minPL, maxPL)
+
+        var cName = god.Name
+
+        g.innerHTML = cBar + cBarPL + cName
+        healths.appendChild(g)
+    }
+}
+function rgbString(r, g, b) {
+    return 'rgb(' + r + ', ' + g + ', ' + b + ')'
+}
