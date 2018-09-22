cc6cdab5513e48541f591ad424e81d56dd4d8569
Jan Klass <kissaki@posteo.de>
2018-09-22 15:00:25 +0200

    Write god data only if it differs from existing data

--- src/pull-gods.jl
+++ src/pull-gods.jl
@@ -1,5 +1,6 @@
 module pull
 
+import CRC32c
 import JSON
 
 include("common.jl")
@@ -13,6 +14,15 @@ if s == nothing
 end
 
 gods = SmiteApi.getgods(s, SmiteApi.Language.English)
-write("data/gods.json", gods)
+newJson = JSON.json(gods)
+crc = CRC32c.crc32c(newJson)
+filename = "data/gods.json"
+existing = open(CRC32c.crc32c, filename)
+if crc != existing
+    @info "Writing new gods data..."
+    write(filename, gods)
+else
+    @info "Pulled god data does not differ from existing stored data."
+end
 
 end
