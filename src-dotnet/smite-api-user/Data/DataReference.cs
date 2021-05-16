3dfeb01730202690ae32979f4bb4ec0aa7b20a54
Jan Klass <kissaki@posteo.de>
2021-05-16 16:52:10 +0200

    Add missing DataReference file
    This was missed while changing code previously

---
+++ src-dotnet/smite-api-user/Data/DataReference.cs
@@ -0,0 +1,40 @@
+﻿namespace KCode.SMITEClient.Data
+{
+    public static class DataReference
+    {
+        /// <summary>
+        /// Ordered list of properties
+        /// </summary>
+        public static readonly string[] ItemProperties = new string[]
+        {
+                "Health",
+                "HP5",
+                "Maximum Health",
+                "Mana",
+                "MP5",
+                "Maximum Mana",
+                "HP5 & MP5",
+                "Movement Speed",
+                "Basic Attack Damage",
+                "Attack Speed",
+                "Cooldown Reduction",
+                "Physical Power",
+                "Magical Power",
+                "Physical Penetration",
+                "Magical Penetration",
+                "Damage Reduction",
+                "Flat Damage Reduction",
+                "Protections",
+                "Physical Protection",
+                "Magical Protection",
+                "Magical Protections",
+                "Crowd Control Reduction",
+                "Physical Lifesteal",
+                "Magical Lifesteal",
+                "Critical Strike Chance",
+                "Physical Critical Strike Chance",
+                "Penetration",
+                "Unlocks at level 10",
+        };
+    }
+}
