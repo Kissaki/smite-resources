d2f353e070fcf7544604b66167af12453a8f617c
Jan Klass <kissaki@posteo.de>
2022-05-14 13:26:52 +0200

    Code: Use newer MD5 API

--- src-dotnet/KCode.SMITEAPI/SignatureCreator.cs
+++ src-dotnet/KCode.SMITEAPI/SignatureCreator.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 
@@ -13,7 +12,7 @@ namespace KCode.SMITEAPI
         private static byte[] Decode(string value) => Encoding.UTF8.GetBytes(value);
 
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Security", "CA5351:Do Not Use Broken Cryptographic Algorithms", Justification = "Required by upstream")]
-        private static byte[] HashMd5(byte[] value) { using var md5 = new MD5CryptoServiceProvider(); return md5.ComputeHash(value); }
+        private static byte[] HashMd5(byte[] value) { using var md5 = MD5.Create(); return md5.ComputeHash(value); }
         private static string ToHexString(byte[] value) => string.Join("", value.Select(x => x.ToString("x2", provider: null)));
     }
 }
